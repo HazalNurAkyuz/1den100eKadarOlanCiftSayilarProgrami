b05a4cbbd6cc8cdad98f4814153af67edc700d90
Hazal Nur Akyuz <hazalnurakyuz@outlook.com>
2022-08-17 14:26:15 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int i = 1;
+        while (i <= 100) {
+
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
     }
 }
